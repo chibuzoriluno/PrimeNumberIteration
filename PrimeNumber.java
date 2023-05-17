ba69cb4688bfb8d93f69f0957df2d849848d96e1
chibuzoriluno <92526675+chibuzoriluno@users.noreply.github.com>
2023-05-17 04:30:04 +0300

    Update PrimeNumber.java

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,62 +1,113 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Random;
+import java.util.Scanner;
 
-public class PrimeTest {
-    private static Random random = new Random();
-
-    public static void main(String[] args) {
-        int num = 12;
-        int iterations = 10; // Number of iterations for the Fermat test
-
-        // Call the isPrime method and retrieve the result and iteration count
-        PrimeResult result = isPrime(num, iterations);
-
-        if (result.isPrime) {
-            System.out.println(num + " is a prime number.");
-        } else {
-            System.out.println(num + " is not a prime number.");
+public class primeNumber{
+    
+    private static void findFactors(int number) {
+        StringBuilder factors = new StringBuilder();
+        int counter = 0;
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                if (factors.length() > 0) {
+                    factors.append(",");
+                }
+                factors.append(i);
+            }
+            counter++;
         }
-        System.out.println("Number of iterations: " + result.iterations);
-    }
 
-    private static PrimeResult isPrime(int n, int k) {
-        if (n < 2)
+        if (factors.length() > 0) {
+            System.out.println(number + " is a composite number and its factors are -> " + factors.toString());
+            System.out.println("With 1st method, number of iteration  is: " + counter);
+        } else {
+            System.out.println(number + " is a prime number and its factors are -> " + factors.toString());
+            System.out.println("With 1st method, number of iteration  is: " + counter);
+        }
+    }
+    
+     public static PrimeResult isPrime(int n, int k) {
+        if (n <= 1 || n == 4)
             return new PrimeResult(false, 0);
-        if (n == 2 || n == 3)
+        if (n <= 3)
             return new PrimeResult(true, 0);
-        int a = getRandomNumber(2, n - 2);
+
+        // Find r and d such that n - 1 = 2^r * d
+        
+        int d = n - 1;
+        int r = 0;
+        while (d % 2 == 0) {
+            
+            d /= 2;
+            r++;
+        }
+
+        // Perform Miller-Rabin test k times
         int iterations = 0;
+        Random random = new Random();
+        int a = 2 + random.nextInt((int) (n - 2));
         for (int i = 0; i < k; i++) {
             
-            int x = modularExponentiation(a, n - 1, n);
+            int x = modularExponentiation(a, d, n);
+            iterations ++;
+            if (x == 1 || x == n - 1)
+                continue;
 
-            iterations++; // Increment iteration count for each iteration
+            boolean isComposite = true;
+            for (int j = 1; j < r; j++) {
+                x = modularExponentiation(x, 2, n);
+                iterations ++;
+                if (x == n - 1) {
+                    isComposite = false;
+                    break;
+                }
+            }
 
-            if (x != 1)
+            if (isComposite)
                 return new PrimeResult(false, iterations);
         }
 
-        return new PrimeResult(true, iterations); // Assign the final iteration count
+        return new PrimeResult(true, iterations);
     }
 
-    private static int modularExponentiation(int base, int exponent, int modulus) {
+    // Modular exponentiation: (a^b) % n
+    public static int modularExponentiation(int a, int b, int n) {
         int result = 1;
-        base %= modulus;
+        a = a % n;
 
-        while (exponent > 0) {
-            if (exponent % 2 == 1)
-                result = (result * base) % modulus;
-            base = (base * base) % modulus;
-            exponent /= 2;
+        while (b > 0) {
+            if (b % 2 == 1)
+                result = (result * a) % n;
+            b /= 2;
+            a = (a * a) % n;
         }
 
         return result;
     }
 
-    private static int getRandomNumber(int min, int max) {
-        return random.nextInt((max - min) + 1) + min;
-    }
+    // Find lowest common factors of a composite number
+    public static HashSet<Integer> findLowestFactors(int n) {
+        HashSet<Integer> factors = new HashSet<>();
 
-    // Custom class to store the result and iteration count
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            while (n % i == 0) {
+                factors.add(i);
+                n /= i;
+            }
+        }
+
+        if (n > 1) {
+            factors.add(n);
+        }
+
+        return factors;
+    }
+    
     private static class PrimeResult {
         boolean isPrime;
         int iterations;
@@ -66,4 +117,71 @@ public class PrimeTest {
             this.iterations = iterations;
         }
     }
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+        do {
+            System.out.println("Menu:");
+            System.out.println("1. Find Prime Number and Factors");
+            
+            System.out.println("2. Exit");
+            System.out.print("Enter your choice: ");
+            choice = scanner.nextInt();
+
+            switch (choice) {
+                
+
+                case 1:
+                    System.out.print("Enter a number: ");
+                    int num = scanner.nextInt();
+                    
+                    findFactors(num);
+                    
+                    int iterations = 10; // Number of iterations for the Miller-Rabin test
+                    boolean isPrime;
+                    int iterationCount;
+
+                    // Call the isPrime method and retrieve the result and iteration count
+                    PrimeResult result = isPrime(num, iterations);
+                    isPrime = result.isPrime;
+                    iterationCount = result.iterations;
+
+                    if (isPrime) {
+                        //System.out.println(num + " is a prime number.");
+                        System.out.println("With 2nd method, number of iteration  is: " + iterationCount);
+                    } else {
+                        //System.out.println(num + " is a composite number.");
+                        System.out.println("With 2nd method, number of iteration  is: " + iterationCount);
+                        //HashSet<Integer> factorrs = findLowestFactors(num);
+                        //ArrayList<Integer> factorList = new ArrayList<>(factorrs);
+                        //System.out.print("Factors: ");
+                        //for (int i = 0; i < factorList.size(); i++) {
+                          //  System.out.print(factorList.get(i));
+                            //if (i < factorrs.size() - 1) {
+                              //  System.out.print(", ");
+                            //}
+                        //}
+                    }
+                        System.out.println();
+                    
+                    break;
+
+                case 2:
+                    System.out.println("Exiting the program. Goodbye!");
+                    break;
+
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+                    break;
+                    
+            }
+        }
+         while (choice != 2);
+
+        scanner.close();
+        
+        }
+        
+        
 }
